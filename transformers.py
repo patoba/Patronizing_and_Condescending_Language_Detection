7f49f08228fdb74c4b1348fa9af80d4dee816cfd
poyhugo <hugo_kaka22@hotmail.com>
2021-11-24 19:40:22 -0700

    Update transformers.py

--- transformers.py
+++ transformers.py
@@ -2,6 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 from nltk.stem import PorterStemmer, WordNetLemmatizer 
+from sklearn.utils.validation import check_is_fitted
 
 class RemoveWords(BaseEstimator, TransformerMixin):
     def __init__(self, unwanted_words = stopwords.words("english")):
@@ -15,52 +16,50 @@ class RemoveWords(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y=None):
+        check_is_fitted(self)
         X_ = X.copy()
         X_ = X_.apply(lambda line: [w for w in line if not w.lower() in self.unwanted_words])
         return X_
-
+all_tag_list = {'CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD',
+                'NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR',
+                'RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ',
+                'WDT','WP','WP$','WRB','RBS'}    
+common_tag_list = {'JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ'}
 class GetTags(BaseEstimator, TransformerMixin):
-    def __init__(self,tag_list=['JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ'],use_all_tags=False):
+    def __init__(self, tag_list = common_tag_list):
         self.tag_list = tag_list
-        self.all_tags = set(['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD',
-                             'NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR','RBS',
-                            'RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ',
-                            'WDT','WP','WP$','WRB'])
-        #self.default_tags_dic = {'ADJ':['JJ','JJR','JJS'],'VERB':['VB','VBD','VBG','VBN','VBP','VBZ']}
-        self.use_all_tags = use_all_tags
-        if self.use_all_tags and len(tag_list)>0:
-            print("Warning: input tag list will be ignored") 
+        if not tag_list.issubset(all_tag_list):
+            raise Warning("tag list provided does not match, look for the correct Peen Tree Bank POS tags")
         
     def fit(self, X, y=None):
         print("POS tags to be used: {}".format(self.tag_list))
         return self
     
     def transform(self, X, y=None):
-        if set(self.tag_list).issubset(self.all_tags) and not self.use_all_tags:
-            X_ = X.copy()
-            X_ = X_.apply(lambda line: pos_tag(line))
-            X_ = X_.apply(lambda line: [w for w, pos in line if pos in self.tag_list])
-        elif self.use_all_tags:
-            X_ = X.copy()
-        else:
-            raise Warning("tag list provided does not match, look for the correct Peen Tree Bank POS tags")
+        check_is_fitted(self)
+        X_ = X.copy()
+        X_ = X_.apply(lambda line: pos_tag(line))
+        X_ = X_.apply(lambda line: [w for w, pos in line if pos in self.tag_list])
         return X_
+
 class GetBase(BaseEstimator, TransformerMixin):
-    def __init__(self, transf= None):
+    def __init__(self, transf = 'Stemming'):
         self.transf = transf
-    
-    def transform(self, X, y = None):
-         words = X.copy
-         if self.transf=='Lematizar' or self.transf=='Lemmatization':
-            lemmatizer = WordNetLemmatizer()
-            words = words.apply(lambda line: [lemmatizer.lemmatize(w) for w in line])
-         elif self.transf=='Stemming' or self.transf=='Enraizar':
-            ps = PorterStemmer()
-            words = words.apply(lambda line: [ps.stem(w) for w in line])
-         else:
+        if transf not in {'Lematizar','Lemmatization','Stemming', 'Enraizar'}:
             raise Warning("""'Lematizar','Lemmatization','Stemming' and 'Enraizar' are the only
             possible options for transf paramater.""")
+    
+    def transform(self, X, y = None):
+         check_is_fitted(self)
+         words = X.copy()
+         words = words.apply(lambda line: [self.transf_f_(w) for w in line])
          return words
     
     def fit(self, X, y):
+        if self.transf == 'Lematizar' or self.transf == 'Lemmatization':
+            lemmatizer = WordNetLemmatizer()
+            self.transf_f_ = lemmatizer.lemmatize  
+        elif self.transf == 'Stemming' or self.transf == 'Enraizar':
+            ps = PorterStemmer()
+            self.transf_f_ = ps.stem
         return self
