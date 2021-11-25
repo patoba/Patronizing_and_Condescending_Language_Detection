05e14effd50483d1dfc6e089dd27414031a67d25
Antonio Avilix Hernández <38363201+antonioavilix@users.noreply.github.com>
2021-11-25 02:22:44 -0600

    Add files via upload

---
+++ Vectorization_Models/word2vec.py
@@ -0,0 +1,40 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from gensim.models import KeyedVectors
+import numpy as np
+import pickle
+
+subset_file = "/datasets/en_embeddings.p"
+en_embeddings = KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', 
+                                                  binary = True, limit = 100000)
+en_embeddings_subset = pickle.load(open(subset_file, "rb"))
+
+class GetSentenceEmbedding(BaseEstimator, TransformerMixin):
+    def __init__(self, embedding_dic = en_embeddings, method=np.sum, 
+                 replace = False):
+        """
+        embedding_dic: Dictionary or gensim KeyedVectors with the word as key and its embedding vector as value.
+        method: Decide how to aggregate word vectors for a sentence, np.sum or np.mean.
+        replace: If true, replace words not found in embedding_dic with word "unknown" otherwise they are not considered
+        """
+        self.embedding_dic = embedding_dic
+        self.method = method
+        self.replace = replace
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        #check_is_fitted(self)
+        X_ = X.copy()
+        if self.replace:
+            X_ = X_.apply(lambda line: 
+                          [self.embedding_dic[word] 
+                           if word in self.embedding_dic 
+                           else self.embedding_dic['unknown']
+                          for word in line])
+        else:
+            X_ = X_.apply(lambda line: [self.embedding_dic[word] for word in line if word in self.embedding_dic])
+        X_ = X_.apply(lambda line: self.method(np.array(line), axis=0))
+        X_ = np.array(X_.to_list())
+        return X_
